aef64a38c983d87655b58f0aa3a4b38ea5beb7a8
NuraidaKashmin <nuraidakkashmin@gmail.com>
2024-11-04 23:31:10 +0600

    banner section complete

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,17 +1,15 @@
 /* eslint-disable react/prop-types */
-import Banner from "../../public/assets/banner.jpg"
 
-const Heading = ({ title, subtitle, button }) => {
+const Heading = ({ title, subtitle, button, imageSection }) => {
+    
     return (
-        <div className=''>
+        <div className='relative'>
             <div className="flex flex-col w-full justify-center items-center bg-[#9538e2]">
                 <h1 className='text-xl md:text-2xl lg:text-4xl font-bold text-white mb-6 mt-20 w-2/3 text-center'>{title}</h1>
                 <p className='text-xs md:text-base text-white w-2/3 text-center mb-6'>{subtitle}</p>
                 <button className="btn rounded-full mb-36">{button}</button>
             </div>
-            <div>
-                <img src={Banner} alt="banner" />
-            </div>
+            <div>{imageSection}</div>   
         </div>
     );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,20 @@
 import Heading from "../components/Heading";
 
 const Home = () => {
+    const imageSection = (
+        <div className='absolute top-3/4 left-1/2 transform -translate-x-1/2 p-4 border-white border-2 rounded-3xl'>
+            <img className="border rounded-3xl w-full" src='../../public/assets/banner.jpg' alt="banner" />
+        </div>
+    )
+
     return (
         <div>
             {/* Heading */}
-            <Heading title={'Upgrade Your Tech Accessorize with Gadget Heaven Accessories'} subtitle={'Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!'} button={'Shop Now'} ></Heading>
+            <Heading title={'Upgrade Your Tech Accessorize with Gadget Heaven Accessories'} subtitle={'Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!'} button={'Shop Now'} imageSection={imageSection}></Heading>
             {/* Dynamic nested component */}
-            
-            
+
+
+
         </div>
     );
 };
